aa2f228c9e7ca07c12203ae27fbcf4d53dec2c28
Gabriel <gabrieldeabreuesposito@gmail.com>
2023-12-14 16:01:33 -0300

    dia-14-12-2023

---
+++ scripts/MessageBox.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const messages = document.querySelectorAll(".ballon");
+    const smart = document.querySelector(".smartphone");
+    const explanation = document.querySelector(".Explanation");    
+        setTimeout(() => {
+            explanation.style.opacity = 1;
+            explanation.style.transform = "translateX(0)"
+            smartphone();
+        }, 500)
+        function smartphone()
+        {
+            setTimeout(() => {
+                smart.style.opacity = 1;
+                smart.style.transform = "translateX(0)";
+                ballons()
+            }, 800)
+        }
+        function ballons()
+        {
+            messages.forEach((message, index) => {
+            //adiciona uma classe para iniciar a anim
+            setTimeout(() => {
+                message.style.opacity = 1;
+                message.style.transform = "translateX(0)";
+            }, index * 1200)
+        });
+        }
+});
--- scripts/lista.js
+++ scripts/lista.js
@@ -13,6 +13,12 @@ function Lista(){
 function zap(){
     window.location.href = "https://wa.me/5532999820536"
 }
+function email (){
+    window.location.href = "mailto:pirasegmg@gmail.com"
+}
+function facebook (){
+    window.location.href = "https://www.facebook.com/people/Piraseg-Consultoria/100093117411278/?mibextid=ZbWKwL"
+}
 function piraseg(){
     window.location.href = "https://pirasegmg.formasegead.com.br/"
 }
